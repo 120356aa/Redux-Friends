dc43f43ebae83581d115ec24492a3f90894a2190
120356aa <yurlovandrew@gmail.com>
2019-02-07 19:37:10 -0800

    moved app

--- friends/src/index.js
+++ friends/src/index.js
@@ -6,8 +6,8 @@ import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 
 import './index.css';
-import App from './App';
-import combineReducers from './reducers'
+import App from './components/App';
+import rootReducer from './reducers'
 
-const store = createStore( combineReducers, applyMiddleware(thunk, logger));
+const store = createStore( rootReducer, applyMiddleware(thunk, logger));
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
